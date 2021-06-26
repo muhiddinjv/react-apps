6025fd1cdcfb53d535be6455ddb076f10c09f937
Emuallim <muhiddin001@gmail.com>
2021-06-27 00:30:07 +0500

    show current time

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,25 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const getBtnTxt = () => { return 'Worked!'; }
+const getTime = () => {
+  return new Date().toLocaleTimeString();
+};
 
 const App = () => {
-
-  const style = {backgroundColor:'grey', color:'white'};
-  const btnTxt = { text: 'click me' };
+  const style = { backgroundColor: "grey", color: "white" };
+  const btnTxt = { text: "click me" };
   // const arr = ['hi','there'];
-  const clicked=(e)=>{alert(e.target)};
+  const clicked = (e) => {
+    alert(e.target);
+  };
 
   return (
-    <div style={{textAlign:'center'}}>
-      <label className="label" htmlFor="name">Enter name: </label>
+    <div style={{ textAlign: "center" }}>
+      <h3>Current Time: {getTime()}</h3>
+
+      <label className="label" htmlFor="name">
+        Enter name:{" "}
+      </label>
+
       <input id="name" type="text" />
-      <button onClick={clicked} style={style}>{btnTxt.text}</button>
-      {/* do not use {btnTxt} bc jsx cannot print out objects */}
-      {/* use {btnTxt.text} instead so that jsx can understand it */}
+
+      <button onClick={clicked} style={style}>
+        {btnTxt.text}
+        {/* do not use {btnTxt} bc jsx cannot print objects */}
+        {/* use {btnTxt.text} so that jsx can understand it */}
+      </button>
     </div>
   );
-
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
