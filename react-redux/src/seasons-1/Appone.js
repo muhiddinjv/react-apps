50e348eb121b7410a7096db3e0225485321251c8
Emuallim <muhiddin001@gmail.com>
2021-06-29 09:35:54 +0500

    init: state object

--- react-redux/src/seasons-1/Appone.js
+++ react-redux/src/seasons-1/Appone.js
@@ -1,18 +1,40 @@
 import React from "react";
 import Season from "./Season";
 
-const Appone = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err), 
-  );
+// const Appone = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err),
+//   );
 
-  return (
-    <div style={{ border: "1px solid grey" }}>
-      <h1>Appone</h1>
-      <Season />
-    </div>
-  );
-};
+//   return (
+//     <div style={{ border: "1px solid grey" }}>
+//       <h1>Appone</h1>
+//       <Season />
+//     </div>
+//   );
+// };
+
+class Appone extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+    };
+  }
+  render() { // React says "You MUST define render"
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return (
+      <div style={{ border: "1px solid grey" }}>
+        <h1>class app</h1>
+        <Season />
+      </div>
+    );
+  }
+}
 
 export default Appone;
