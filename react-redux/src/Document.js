31d09b763dc53ec08313bdc1b98726174a0b8a9b
Emuallim <muhiddin001@gmail.com>
2021-06-29 16:13:31 +0500

    refactor: use state w/o constructor

--- react-redux/src/Document.js
+++ react-redux/src/Document.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import Placeholder from './Placeholder';
-import Appone from './seasons-1/Appone';
+import React from "react";
+import Placeholder from "./Placeholder";
+import Appone from "./seasons-1/Appone";
 
-const Document = (props) =>{
-    return(
-        <div>
-            <Placeholder>
-                <div className="ui icon header">
-                    <i className="pd file outline icon"></i>
-                    <Appone />
-                </div>
-                <div className="ui primary button">Add Document</div>
-            </Placeholder>
-            <Placeholder>
-                <h4 className="ui header">For your information</h4>
-                <p>lorem empsum which is supposed to be a long line of text which almost never ends but for some reason it is not working. No wait. I think I know the reason. I have not installed the extension yet.</p>
-            </Placeholder>
+const Document = (props) => {
+  return (
+    <div>
+      <Placeholder>
+        <div className="ui icon header">
+          <i className="pd file outline icon"></i>
+          <Appone />
         </div>
-    )
-}
+        <div className="ui primary button">Add Document</div>
+      </Placeholder>
+      <Placeholder>
+        <h4 className="ui header">Application Lifecycle - Document Component</h4>
+        <ol>
+          <li>JS file loaded by browser</li>
+          <li>Instance of App component is created</li>
+          <li>App component 'constructor' function gets called</li>
+          <li>
+            State object is created and assigned to the 'this.state' property
+          </li>
+          <li>We call geolocation service</li>
+          <li>React calls the components render method</li>
+          <li>App returns JSS, gets rendered to page as HTML</li>
+          <li>... wait ...</li>
+          <li>We get result of geolocation!</li>
+          <li>We update our state object with a call 'this.setState'</li>
+          <li>React sees that we updated the state of a component</li>
+          <li>React calls our 'render' method a second time</li>
+          <li>Render method returns some (uploaded) JSX</li>
+          <li>React takes that JSX and updates content onscreen</li>
+        </ol>
+      </Placeholder>
+    </div>
+  );
+};
 
 export default Document;
--- react-redux/src/seasons-1/Appone.js
+++ react-redux/src/seasons-1/Appone.js
@@ -1,11 +1,9 @@
 import React from "react";
 
 class Appone extends React.Component {
-  constructor(props) {
-    super(props);
-    //This is the ONLY time we do direct assignment to this.state
-    this.state = { lat: null, errorMsg: "" };
-  }
+  //This is the ONLY time we do direct assignment to this.state
+  state = { lat: null, errorMsg: "" };
+  // init state w/o constructor method
 
   componentDidMount() {
     //if put in render, it'd double fetch location
@@ -27,30 +25,7 @@ class Appone extends React.Component {
     if (!this.state.errorMsg && this.state.lat) {
       return <div>Error: {this.state.lat}</div>;
     }
-
-    return (
-      <div style={{ border: "1px solid grey" }}>
-        <ol>
-          <li>Loading... </li>
-          <li>JS file loaded by browser</li>
-          <li>Instance of App component is created</li>
-          <li>App component 'constructor' function gets called</li>
-          <li>
-            State object is created and assigned to the 'this.state' property
-          </li>
-          <li>We call geolocation service</li>
-          <li>React calls the components render method</li>
-          <li>App returns JSS, gets rendered to page as HTML</li>
-          <li>... wait ...</li>
-          <li>We get result of geolocation!</li>
-          <li>We update our state object with a call 'this.setState'</li>
-          <li>React sees that we updated the state of a component</li>
-          <li>React calls our 'render' method a second time</li>
-          <li>Render method returns some (uploaded) JSX</li>
-          <li>React takes that JSX and updates content onscreen</li>
-        </ol>
-      </div>
-    );
+    return <div>Loading... "Appone" </div>
   }
 }
 
