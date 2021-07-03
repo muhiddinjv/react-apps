0e26ed684d42e9a042bf213c2fb3cac65e9de256
Emuallim <muhiddin001@gmail.com>
2021-07-04 00:09:47 +0500

    feat: view axios request results

--- react-redux/src/events-2/apptwo.js
+++ react-redux/src/events-2/apptwo.js
@@ -1,10 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 import Placeholder from "../comps-0/Placeholder";
 
 class AppTwo extends React.Component {
   onSearchSubmit(term){
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos',{
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID _rKDnAdQlKexqAsytbYe6pr0MbSm5IHs5sMfX2_eABs'
+      }
+    });
   }
 
   render() {
@@ -13,7 +19,7 @@ class AppTwo extends React.Component {
         <Placeholder>
           <SearchBar onSubmit={this.onSearchSubmit} />
           {/* you can write "cocaCola" instead of "onSubmit" but*/}
-          {/* You cant do the same on a JSX element e.g.<form> */}
+          {/* You cannot do the same on a JSX element e.g.<form> */}
         </Placeholder>
       </div>
     );
