d2a48587fcee070a6c59da4f50799826a637049f
Emuallim <muhiddinjv@gmail.com>
2022-01-15 09:55:43 +0500

    feat: add calims & accounting reducers

---
+++ react-redux/src/redux/Redux.js
@@ -0,0 +1,53 @@
+//action creators (customers dropping off forms)
+const createPolicy = (name, amount) => {
+  return {
+    type: "CREATE_POLICY",
+    payload: {
+      name: name,
+      amount: amount,
+    },
+  };
+};
+
+const deletePolicy = (name) => {
+  return {
+    type: "DELETE_POLICY",
+    payload: {
+      name: name,
+    },
+  };
+};
+
+const createClaim = (name, moneyToGet) => {
+  return {
+    type: "CREATE_CLAIM",
+    payload: {
+      name: name,
+      moneyToGet: moneyToGet,
+    },
+  };
+};
+
+
+// REDUCERS (departments) ------------------------
+const claims = (oldListOfClaims = [], action) => {
+  if (action.type === 'CREATE_CLAIM'){
+    // we care about this action (form)
+    // 1. oldListOfClaims.push(action.payload) = BAD b/c it changes the original array
+    // 2. return [...oldListOfClaims, action.payload] = GOOD b/c it creates a new array. ALWAYS USE ...THIS
+    return [...oldListOfClaims, action.payload] //joins 2 arrs
+  } else {
+    // we don't care about this action (form)
+    return oldListOfClaims;
+  }
+}
+
+const accounting = (bagOfMoney = 100, action) => {
+  if (action.type === 'CREATE_CLAIM'){
+    return bagOfMoney - action.payload.moneyToGet
+  } else if (action.type === 'CREATE_POLICY') {
+    return bagOfMoney + action.payload.moneyToGet;
+  } else {
+    return bagOfMoney;
+  }
+}
