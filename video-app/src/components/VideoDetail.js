cbfa0ecca07072201fad6642b0000ec36855f7ed
Emuallim <muhiddinjv@gmail.com>
2022-02-25 21:37:02 +0500

    embed youtube video & re-style App

--- video-app/src/components/VideoDetail.js
+++ video-app/src/components/VideoDetail.js
@@ -4,12 +4,17 @@ const VideoDetail = ({ video }) => {
   
   if (!video){return <div>Loading...</div>}
   const { title, description } = video.snippet;
-
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
 
   return (
-    <div className='ui segment'>
-      <h4 className="ui header">{title}</h4>
-      <p>{description}</p>
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} title={title}/>
+      </div>
+      <div className='ui segment'>
+        <h4 className="ui header">{title}</h4>
+        <p>{description}</p>
+    </div>
     </div>
   )
 }
--- video-app/src/components/VideoItem.js
+++ video-app/src/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = ({ video, onVideoSelect }) => {
     const {title, thumbnails} = video.snippet
   return (
     <div onClick={()=>onVideoSelect(video)} className='video-item item'>
-        <img className='ui image' src={thumbnails.medium.url} alt="" />
+        <img className='ui image' src={thumbnails.medium.url} alt={title} />
         <div className="content">
             <div className="header">{title}</div>
             <div className="description">Video desc</div>
