168c65b7f4583bfecbaa7e9c866d0abb854561b1
Emuallim <muhiddinjv@gmail.com>
2022-04-26 14:21:56 +0500

    feat: add redux-thunk to fetch data thru actions

--- apps/src/index.js
+++ apps/src/index.js
@@ -4,11 +4,14 @@ import Apps from "./components/Apps";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 // import { configureStore } from "@reduxjs/toolkit";
-import { legacy_createStore as createStore} from 'redux'
+import { legacy_createStore as createStore, applyMiddleware} from 'redux'
+import thunk from "redux-thunk";
 import reducers from "./redux/reducers";
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
       <Apps />
     </BrowserRouter>
--- apps/src/redux/actions/index.js
+++ apps/src/redux/actions/index.js
@@ -1,3 +1,10 @@
-export const fetchPosts = () => {
-    return { type: "FETCH_POSTS" }
+import jsonPlaceholder from '../../api/jsonPlaceholder'
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    return { type: "FETCH_POSTS", response }
+}
+
+export const selectPost = () => {
+    return { type: "SELECT_POST" }
 };
