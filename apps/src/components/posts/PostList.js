801429aaea6b7087f2b5d148a7ce9c07f2593562
Emuallim <muhiddinjv@gmail.com>
2022-04-30 15:18:18 +0500

    feat: add show/hide buttons

--- apps/src/components/posts/PostList.js
+++ apps/src/components/posts/PostList.js
@@ -8,13 +8,15 @@ import uCanMutateState from '../../assets/uCanMutateState.jpg';
 import howToUpdateState from '../../assets/howToUpdateState.jpg';
 
 class PostList extends Component {
+  state = {showImages: false, showList: false,};
+
   componentDidMount(){
       this.props.fetchPosts();
   }
   renderList() {
     return this.props.posts.map(post => {
       return (
-        <div className="item" key={post.id}>
+        <div className="item" key={post.id} style={{display: this.state.showList ? 'block' : 'none'}}>
           <i className="large middle aligned icon user" />
           <div className="content">
             <div className="description">
@@ -26,18 +28,29 @@ class PostList extends Component {
       )
     })
   }
+  showList = () => {
+    !this.state.showList ? this.setState({showList: true}) : this.setState({showList: false});
+  }
+
+  showImages = () => {
+    !this.state.showImages ? this.setState({showImages: true}) : this.setState({showImages: false});
+  }
   render() {
     return (
       <div>
           <h1>Post List</h1>
-          <img style={{width:'500px'}} src={dataLoadRedux} alt="redux-data-load"/>
-          <div style={{display:'flex', flexWrap:'wrap', width:'600px'}}>
-            <img src={rulesOfReducers} style={{width:'300px'}} alt="rules-of-reducers"/>
-            <img src={dontMutateState} style={{width:'300px'}} alt="dont-mutate-state"/> 
-            <img src={uCanMutateState} style={{width:'300px'}} alt="can-mutate-state"/>
-            <img src={howToUpdateState} style={{width:'300px'}} alt="how2-mutate-state"/>
-          </div>    
+          <div className="list">
+            <button onClick={this.showImages}>show images</button>
+            <div style={{display: this.state.showImages ? 'flex' : 'none', flexWrap:'wrap', gap:'20px'}}>
+              <img style={{width:'300px'}} src={dataLoadRedux} alt="redux-data-load"/>
+              <img src={rulesOfReducers} style={{width:'300px'}} alt="rules-of-reducers"/>
+              <img src={dontMutateState} style={{width:'300px'}} alt="dont-mutate-state"/> 
+              <img src={uCanMutateState} style={{width:'300px'}} alt="can-mutate-state"/>
+              <img src={howToUpdateState} style={{width:'300px'}} alt="how2-mutate-state"/>
+            </div>   
+          </div>   
           <div className="ui relaxed divided list">
+            <button onClick={this.showList}>show list</button>
             {this.renderList()}
           </div>
       </div>
